0ebaee44918f726c9ccd897520125230dac09c52
melihs <melihsahin24@gmail.com>
2019-05-15 22:58:13 +0300

    kermes_donors pivot tablo adı kermes_donor olarak değiştirildi

--- database/migrations/2019_05_05_195813_create_kermes_donor_table.php
+++ database/migrations/2019_05_05_195813_create_kermes_donor_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateKermesDonorsTable extends Migration
+class CreateKermesDonorTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateKermesDonorsTable extends Migration
      */
     public function up()
     {
-        Schema::create('kermes_donors', function (Blueprint $table) {
+        Schema::create('kermes_donor', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('kermes_id');
             $table->integer('donor_id');
