6157340a9a136b78058d8b4205c113fefd4cb21a
melihs <melihsahin24@gmail.com>
2019-05-20 21:59:49 +0300

    UserFactory password kısmı güncellendi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,15 +16,13 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(User::class, function (Faker $faker) {
-        $token = Str::random(80);
     return [
            'name' => $faker->name,
            'surname' => $faker->lastName,
            'email' => $faker->unique()->safeEmail,
            'email_verified_at' => now(),
-           'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+           'password' => bcrypt(random(10)),
            'remember_token' => Str::random(10),
            'type' => 'donors',
-           'api_token'=> hash('sha256',$token)
     ];
 });
